348593cde2d05c2cda6ae67ecfeaa7d4b17ce8fa
Agus.Dev <agusalanis.dev@gmail.com>
2024-06-25 21:39:56 -0300

    instalando express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
 // Archivo principal
 
+import server from "./server";
 
-
+server.listen(4000, () => {
+    console.log('Backend en el pueto 4000')
+})
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,12 @@
 // Configuracion del servidor
 
+import express from 'express'
+
+const server = express()
+
+// Routing
+server.get('/', (req, res) => {
+    res.send('Hola')
+})
+
+export default server
