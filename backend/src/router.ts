b70951e4d73a1f4de9e5d7b9f425f36a7646a9af
Agus.Dev <agusalanis.dev@gmail.com>
2024-06-26 12:07:29 -0300

    validando la entrada de datos

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createProduct } from './handlers/product'
 
 const router = Router()
@@ -8,7 +9,21 @@ router.get('/', (req, res) => {
     res.json('desde get')
 })
 
-router.post('/', createProduct)
+router.post('/', 
+    
+    // Validacion
+    body('name')
+                .notEmpty().withMessage('El nombre del Producto no puede ir vacio'),
+                
+                
+
+    body('price')
+                .isNumeric().withMessage('Valor no valido')
+                .notEmpty().withMessage('El precio del Producto no puede ir vacio')
+                .custom((value => value > 0)).withMessage('El precio debe ser mayor a cero'),
+                
+    createProduct
+)
 
 router.put('/', (req, res) => {
     res.json('desde put')
