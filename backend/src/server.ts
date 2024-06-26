e2dc7567e9dc9d225af5f4c377709365b23e3b1e
Agus.Dev <agusalanis.dev@gmail.com>
2024-06-25 21:52:15 -0300

    rutas en su propio archivo

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,12 @@
 // Configuracion del servidor
 
 import express from 'express'
+import router from './router'
 
 const server = express()
 
-// Routing
-server.get('/', (req, res) => {
-    res.json('desde get')
-})
+server.use('/api/products', router)
 
-server.post('/', (req, res) => {
-    res.json('desde post')
-})
 
-server.put('/', (req, res) => {
-    res.json('desde put')
-})
-
-server.patch('/', (req, res) => {
-    res.json('desde patch')
-})
-
-server.delete('/', (req, res) => {
-    res.json('desde delete')
-})
 
 export default server
