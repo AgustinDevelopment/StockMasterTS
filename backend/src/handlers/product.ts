e7bf7ea2e1061cb360a316314f8030499af382e3
Agus.Dev <agusalanis.dev@gmail.com>
2024-06-26 11:44:50 -0300

    mejorando la creacion de productos

--- backend/src/handlers/product.ts
+++ backend/src/handlers/product.ts
@@ -3,8 +3,6 @@ import Product from '../models/Product.model'
 
 export const createProduct = async(req : Request, res : Response) => {
 
-    const product = new Product(req.body)
-    const saveProduct = await product.save()  // Almacenarlo en la BD
-
-    res.json({data: saveProduct})
+    const product = await Product.create(req.body) // Almacenar el Producto en la BD
+    res.json({data: product})
 }
