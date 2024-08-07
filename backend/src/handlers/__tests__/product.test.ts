6c179f1a561652911cb31ef2bb697337476a7361
Agus.Dev <agusalanis.dev@gmail.com>
2024-08-07 20:06:45 -0300

    TEST a la validacion para crear producto

--- backend/src/handlers/__tests__/product.test.ts
+++ backend/src/handlers/__tests__/product.test.ts
@@ -3,6 +3,22 @@ import server from '../../server'
 
 // Test POST
 describe('POST /api/products', () => {
+
+    // Validar errores
+    test('display validation errors', async () => {
+        const response = await request(server).post('/api/products').send({}) // Almacenamosla solicitud HTTP usando supertest (producto vacio)
+
+        expect(response.status).toBe(400) // Esperamos que la solicutud sea incorrecta
+        expect(response.body).toHaveProperty('errors') // Comprobamos que la respuesta tenga la propiedad errores
+        expect(response.body.errors).toHaveLength(4) // Verificamos que tengamos 4 errores de validacion
+
+        // Contraparte
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+
+    })
+
+    // Crear nuevo producto
     test('create a new product', async () => {
         const response = await request(server).post('/api/products').send({ // Almacenamosla solicitud HTTP usando supertest
             // Enviamos el objeto JSON
@@ -16,6 +32,6 @@ describe('POST /api/products', () => {
         // Contraparte
         expect(response.status).not.toBe(404) // Comprobamos que el codigo de estado se encuentre
         expect(response.status).not.toBe(200) // Para crear recursos nuevos usamos 201 no 200
-        expect(response.body).not.toHaveProperty('error') // Comprobamos que no hayan errores
+        expect(response.body).not.toHaveProperty('errors') // Comprobamos que no hayan errores
     })
 })
