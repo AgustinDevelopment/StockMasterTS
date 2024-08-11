3f9343f6620dc49aea28ca0ab538a2dfc793386f
Agus.Dev <agusalanis.dev@gmail.com>
2024-08-11 19:17:42 -0300

    routing entre las diferentes paginas

--- frontend/src/views/NewProduct.tsx
+++ frontend/src/views/NewProduct.tsx
@@ -1,7 +1,18 @@
-
+import { Link } from 'react-router-dom'
 
 export default function NewProducts() {
   return (
-    <div>NewProduct</div>
+    <>
+      <div className='flex justify-between'>
+            <h2 className='text-4xl font-black text-slate-700'>Registrar Producto</h2>
+            <Link
+                to="/"
+                className='rounded-md bg-slate-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-slate-500'
+            >
+                Volver a Producto
+            </Link>
+
+        </div>
+    </>
   )
 }
