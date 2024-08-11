90b9fea7dc37eba1e0720972577d0b716a421f76
Agus.Dev <agusalanis.dev@gmail.com>
2024-08-11 19:11:48 -0300

    primeros pasos en el layout

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -3,9 +3,16 @@ import { Outlet } from 'react-router-dom'
 export default function Layaout() {
   return (
     <>
-        <div>Layout</div>
+        <header className='bg-slate-900'>
+          <div className='mx-auto max-w-6xl py-10'>
+            <h1 className='text-4xl font-extrabold text-white'>Administrador de Productos</h1>
+          </div>
+        </header>
 
-        <Outlet />
+        <main className='mt-10 mx-auto max-w-6xl p-10 bg-white shadow'>
+          <Outlet />
+        </main>
+        
     </>
   )
 }
