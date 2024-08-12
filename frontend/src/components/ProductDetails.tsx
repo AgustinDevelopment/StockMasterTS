6b00f713a0aec8c74b02f868972556935f356109
Agus.Dev <agusalanis.dev@gmail.com>
2024-08-12 20:56:34 -0300

    boton para editar el producto

--- frontend/src/components/ProductDetails.tsx
+++ frontend/src/components/ProductDetails.tsx
@@ -1,5 +1,6 @@
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
+import { Link } from "react-router-dom"
 
 type ProductDetailsProps = {
     product : Product
@@ -21,7 +22,12 @@ export default function ProductDetails({product} : ProductDetailsProps) {
             {isAvailable ? 'Disponible' : 'No Disponible'}
         </td>
         <td className="p-3 text-lg text-gray-800 ">
-        
+            <div className="flex gap-2 items-center">
+                <Link
+                    to={`/productos/${product.id}/editar`}
+                    className="bg-slate-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                >Editar</Link>
+            </div>
         </td>
     </tr> 
   )
