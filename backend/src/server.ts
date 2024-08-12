3e681fdd820dad1d30bb8f2a11455847c32e7eff
Agus.Dev <agusalanis.dev@gmail.com>
2024-08-12 14:51:10 -0300

    cambios en el server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -41,7 +41,7 @@ server.use(cors(corsOptions))
 // Leer datos de formularios
 server.use(express.json())
 
-server.use(morgan('dev'))
+// server.use(morgan('dev'))
 
 server.use('/api/products', router)
 
