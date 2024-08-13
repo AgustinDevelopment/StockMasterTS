2177cb4e11208136ae646c35b089634a5ae6d6ca
Agus.Dev <agusalanis.dev@gmail.com>
2024-08-13 12:36:45 -0300

    ajustes carpera dist

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,13 @@
 "use strict";
 // Archivo principal
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-(0, server_1.sumar)();
-console.log('desde index.ts');
+const server_1 = __importDefault(require("./server"));
+const colors_1 = __importDefault(require("colors"));
+const port = process.env.PORT || 4000;
+server_1.default.listen(port, () => {
+    console.log(colors_1.default.bgCyan(`REST API en el puesto ${port}`));
+});
 //# sourceMappingURL=index.js.map
--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,8 +1,48 @@
 "use strict";
 // Configuracion del servidor
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sumar = sumar;
-function sumar() {
-    console.log(2 + 3);
+exports.connectDB = connectDB;
+const express_1 = __importDefault(require("express"));
+const colors_1 = __importDefault(require("colors"));
+const cors_1 = __importDefault(require("cors"));
+const router_1 = __importDefault(require("./router"));
+const db_1 = __importDefault(require("./config/db"));
+// Conectar a la BD
+async function connectDB() {
+    try {
+        await db_1.default.authenticate();
+        db_1.default.sync();
+        // console.log(colors.bgBlue.white('Conexion Exitosa a la BD'))
+    }
+    catch (error) {
+        // console.log(error)
+        console.log(colors_1.default.bgRed.white('Hubo un error al conectar a la BD'));
+    }
 }
+connectDB();
+// Instancia de express
+const server = (0, express_1.default)();
+// Permitir conexiones
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (origin === process.env.FRONTEND_URL || !origin) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error('Error de CORS'));
+        }
+    }
+};
+server.use((0, cors_1.default)(corsOptions));
+// Leer datos de formularios
+server.use(express_1.default.json());
+// server.use(morgan('dev'))
+server.use('/api/products', router_1.default);
+server.get('/api', (req, res) => {
+    res.json({ msg: 'Desde API' });
+});
+exports.default = server;
 //# sourceMappingURL=server.js.map
